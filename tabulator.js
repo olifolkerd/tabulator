05c9d03417c387c66f0af727683d0cba8c72a0ba
Oli Folkerd <oliver.folkerd@gmail.com>
2016-01-10 22:02:14 +0000

    code tidy

--- tabulator.js
+++ tabulator.js
@@ -1420,9 +1420,7 @@ editors:{
 		var starInactive = $('<svg width="' + size + '" height="' + size + '" class="tabulator-star-inactive"  viewBox="0 0 512 512" xml:space="preserve" style="padding:0 1px;"><polygon fill="#010155" stroke="#686868" stroke-width="37.6152" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="259.216,29.942 330.27,173.919 489.16,197.007 374.185,309.08 401.33,467.31 259.216,392.612 117.104,467.31 144.25,309.08 29.274,197.007 188.165,173.919 "/></svg>');
 
 		for(i=1;i<= maxStars;i++){
-
 			var nextStar = i <= value ? starActive : starInactive;
-
 			stars.append(nextStar.clone());
 		}
 
