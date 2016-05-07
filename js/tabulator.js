5a08a3509731c916d43fcf329f7d1f38db831ec5
Oli Folkerd <oliver.folkerd@gmail.com>
2016-05-07 12:50:18 +0100

    updated to latest version of tabulator

--- js/tabulator.js
+++ js/tabulator.js
@@ -1924,7 +1924,6 @@
 			return "<a href='" + value + "'>" + value + "</a>";
 		},
 		tick:function(value, data, cell, row, options, formatterParams){
-			console.log("font", cell.closest(".tabulator").css("font-size"))
 			var tick = '<svg enable-background="new 0 0 24 24" height="14" width="14"  viewBox="0 0 24 24" xml:space="preserve" ><path fill="#2DC214" clip-rule="evenodd" d="M21.652,3.211c-0.293-0.295-0.77-0.295-1.061,0L9.41,14.34  c-0.293,0.297-0.771,0.297-1.062,0L3.449,9.351C3.304,9.203,3.114,9.13,2.923,9.129C2.73,9.128,2.534,9.201,2.387,9.351  l-2.165,1.946C0.078,11.445,0,11.63,0,11.823c0,0.194,0.078,0.397,0.223,0.544l4.94,5.184c0.292,0.296,0.771,0.776,1.062,1.07  l2.124,2.141c0.292,0.293,0.769,0.293,1.062,0l14.366-14.34c0.293-0.294,0.293-0.777,0-1.071L21.652,3.211z" fill-rule="evenodd"/></svg>';
 
 			if(value === true || value === 'true' || value === 'True' || value === 1){
@@ -1981,13 +1980,14 @@
 
 			//workout percentage
 			var percent = (max - min) / 100;
-			value = Math.round((value - min) / percent);
+			value = 100 - Math.round((value - min) / percent);
 
 			cell.css({
 				"min-width":"30px",
+				"position":"relative",
 			});
 
-			return "<div style='margin-top:3px; height:10px; width:" + value + "%; background-color:" + color + "; display:inline-block; vertical-align:middle;' data-max='" + max + "' data-min='" + min + "'></div>"
+			return "<div style='position:absolute; top:8px; bottom:8px; left:4px; right:" + value + "%; margin-right:4px; background-color:" + color + "; display:inline-block;' data-max='" + max + "' data-min='" + min + "'></div>"
 		},
 		color:function(value, data, cell, row, options, formatterParams){
 			cell.css({"background-color":value});
@@ -2158,7 +2158,7 @@
 
 			//workout percentage
 			var percent = (max - min) / 100;
-			value = Math.round((value - min) / percent);
+			value = 100 - Math.round((value - min) / percent);
 
 			cell.css({
 				padding:"0 4px",
@@ -2169,9 +2169,9 @@
 				cell.trigger("editval", newval);
 			}
 
-			var bar = $("<div style='margin-top:7px; height:10px; width:" + value + "%; background-color:#488CE9; display:inline-block; vertical-align:middle; position:relative; max-width:100%; min-width:0%;' data-max='" + max + "' data-min='" + min + "'></div>");
+			var bar = $("<div style='position:absolute; top:8px; bottom:8px; left:4px; right:" + value + "%; margin-right:4px; background-color:#488CE9; display:inline-block; max-width:100%; min-width:0%;' data-max='" + max + "' data-min='" + min + "'></div>");
 
-			var handle = $("<div class='taulator-progress-handle' style='position:absolute; right:0; top:0; bottom:0; width:5px;'></div>");
+			var handle = $("<div class='tabulator-progress-handle' style='position:absolute; right:0; top:0; bottom:0; width:5px;'></div>");
 
 			bar.append(handle);
 
