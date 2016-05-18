a8f9cffba091a4ac2feca552a530e24d079a72e8
Oli Folkerd <oliver.folkerd@gmail.com>
2016-05-18 03:14:57 +0100

    Added github.io link to title comments

--- tabulator.js
+++ tabulator.js
@@ -5,6 +5,9 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * Full Documentation & Demos can be found at: http://olifolkerd.github.io/tabulator/
+ *
  */
 
  (function(){
