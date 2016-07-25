08a5572a2fb61dc80c556ecb37c7da4994172823
Oli Folkerd <oliver.folkerd@gmail.com>
2016-07-25 21:38:42 +0100

    object scope now passed to sorters, formatters and editors

--- tabulator.js
+++ tabulator.js
@@ -1139,18 +1139,14 @@
 		var el1 = dir == "asc" ? a : b;
 		var el2 = dir == "asc" ? b : a;
 
-		el1 = el1[column.field];
-		el2 = el2[column.field];
-
-		//workaround to format dates correctly
-		a = column.sorter == "date" ? self._formatDate(el1) : el1;
-		b = column.sorter == "date" ? self._formatDate(el2) : el2;
+		a = el1[column.field];
+		b = el2[column.field];
 
 		//run sorter
 		var sorter = typeof(column.sorter) == "undefined" ? "string" : column.sorter;
 		sorter = typeof(sorter) == "string" ? self.sorters[sorter] : sorter;
 
-		return sorter(a, b);
+		return sorter.call(self, a, b);
 	},
 
 	////////////////// Data Pagination //////////////////
@@ -1437,7 +1433,6 @@
 
 		$.each(self.options.columns, function(i, column){
 			//deal with values that arnt declared
-
 			var value = typeof(item[column.field]) == "undefined" ? "" : item[column.field];
 
 			// set empty values to not break search
@@ -1494,7 +1489,7 @@
 						//Load editor
 						var editorFunc = typeof(cell.data("editor")) == "string" ? self.editors[cell.data("editor")] : cell.data("editor");
 
-						var cellEditor = editorFunc(cell, cell.data("value"));
+						var cellEditor = editorFunc.call(self, cell, cell.data("value"));
 
 						//if editor returned, add to DOM, if false, abort edit
 						if(cellEditor !== false){
@@ -2052,10 +2047,12 @@
 
 	//format cell contents
 	_formatCell:function(formatter, value, data, cell, row, formatterParams){
+		var self = this;
+
 		var formatter = typeof(formatter) == "undefined" ? "plaintext" : formatter;
 		formatter = typeof(formatter) == "string" ? this.formatters[formatter] : formatter;
 
-		return formatter(value, data, cell, row, this.options, formatterParams);
+		return formatter.call(self, value, data, cell, row, this.options, formatterParams);
 	},
 
 	////////////////// Table Interaction Handlers //////////////////
@@ -2147,7 +2144,9 @@
 			return String(a).toLowerCase().localeCompare(String(b).toLowerCase());
 		},
 		date:function(a, b){ //sort dates
-			return a - b;
+			var self = this;
+
+			return self._formatDate(a) - self._formatDate(b);
 		},
 		boolean:function(a, b){ //sort booleans
 			var el1 = a === true || a === "true" || a === "True" || a === 1 ? 1 : 0;
