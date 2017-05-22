e5f6f2c0e2ba3df3302b73b920769e80c3ed3ee6
Oli Folkerd <oliver.folkerd@gmail.com>
2017-05-22 07:38:40 +0100

    handle loading of paged data on table build

--- dist/js/tabulator.js
+++ dist/js/tabulator.js
@@ -3713,20 +3713,28 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         //load initial data set
 
 
-        if (self.options.data.length) {
+        if (self.options.pagination && this.extExists("page")) {
 
-          self.rowManager.setData(self.options.data);
+          self.extensions.page.reset(true);
+
+          self.extensions.page.setPage(1);
         } else {
 
-          if (self.options.ajaxURL && this.extExists("ajax")) {
-
-            self.extensions.ajax.sendRequest(function (data) {
-
-              self.rowManager.setData(data);
-            });
-          } else {
+          if (self.options.data.length) {
 
             self.rowManager.setData(self.options.data);
+          } else {
+
+            if (self.options.ajaxURL && this.extExists("ajax")) {
+
+              self.extensions.ajax.sendRequest(function (data) {
+
+                self.rowManager.setData(data);
+              });
+            } else {
+
+              self.rowManager.setData(self.options.data);
+            }
           }
         }
       }
--- src/js/core.js
+++ src/js/core.js
@@ -174,15 +174,20 @@
 	 				self._buildElement();
 
 	 				//load initial data set
-	 				if(self.options.data.length){
-	 					self.rowManager.setData(self.options.data);
+	 				if(self.options.pagination && this.extExists("page")){
+	 					self.extensions.page.reset(true);
+	 					self.extensions.page.setPage(1);
 	 				}else{
-	 					if(self.options.ajaxURL && this.extExists("ajax")){
-	 						self.extensions.ajax.sendRequest(function(data){
-	 							self.rowManager.setData(data);
-	 						});
-	 					}else{
+	 					if(self.options.data.length){
 	 						self.rowManager.setData(self.options.data);
+	 					}else{
+	 						if(self.options.ajaxURL && this.extExists("ajax")){
+	 							self.extensions.ajax.sendRequest(function(data){
+	 								self.rowManager.setData(data);
+	 							});
+	 						}else{
+	 							self.rowManager.setData(self.options.data);
+	 						}
 	 					}
 	 				}
 
