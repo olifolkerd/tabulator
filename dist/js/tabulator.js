07ff86f627b13f4b9af0d2c9f0bbe1f402c80038
Oli Folkerd <oliver.folkerd@gmail.com>
2018-03-24 13:58:13 +0000

    update version to 3.4.6

--- dist/js/tabulator.js
+++ dist/js/tabulator.js
@@ -2,7 +2,7 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-/* Tabulator v3.4.5 (c) Oliver Folkerd */
+/* Tabulator v3.4.6 (c) Oliver Folkerd */
 
 /*
  * This file is part of the Tabulator package.
