2b1d9457a3b4fdff6ffb2c84cd73fc0046d8a841
dpatel4 <patel.davesh@gmail.com>
2018-05-23 08:38:23 +0100

    Check rowVal is valid in 'like' filter.

--- src/js/extensions/filter.js
+++ src/js/extensions/filter.js
@@ -559,7 +559,10 @@ Filter.prototype.filters ={
 		if(filterVal === null || typeof filterVal === "undefined"){
 			return rowVal === filterVal ? true : false;
 		}else{
-			return rowVal.toLowerCase().indexOf(filterVal.toLowerCase()) > -1 ? true : false;
+			if(typeof rowVal != 'undefined' && rowVal)
+				return rowVal.toLowerCase().indexOf(filterVal.toLowerCase()) > -1 ? true : false;
+			else
+				return false;
 		}
 	},
 
