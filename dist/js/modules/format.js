c8dd2ab9cfb948346a0cbf9142971a94a72d3125
Oli Folkerd <oliver.folkerd@gmail.com>
2018-09-23 16:01:31 +0100

    add datetimediff formatter

--- dist/js/modules/format.js
+++ dist/js/modules/format.js
@@ -303,6 +303,35 @@ Format.prototype.formatters = {
 		}
 	},
 
+	datetimediff: function datetime(cell, formatterParams, onRendered) {
+		var inputFormat = formatterParams.inputFormat || "YYYY-MM-DD hh:mm:ss";
+		var invalid = typeof formatterParams.invalidPlaceholder !== "undefined" ? formatterParams.invalidPlaceholder : "";
+		var suffix = typeof formatterParams.suffix !== "undefined" ? formatterParams.suffix : false;
+		var period = typeof formatterParams.period !== "undefined" ? formatterParams.period : undefined;
+		var humanize = typeof formatterParams.humanize !== "undefined" ? formatterParams.humanize : false;
+		var date = typeof formatterParams.date !== "undefined" ? formatterParams.date : moment();
+		var value = cell.getValue();
+
+		var newDatetime = moment(value, inputFormat);
+
+		if (newDatetime.isValid()) {
+			if (humanize) {
+				return moment.duration(newDatetime.diff(date)).humanize(suffix);
+			} else {
+				return newDatetime.diff(date, period) + (suffix ? " " + suffix : "");
+			}
+		} else {
+
+			if (invalid === true) {
+				return value;
+			} else if (typeof invalid === "function") {
+				return invalid(value);
+			} else {
+				return invalid;
+			}
+		}
+	},
+
 	//select
 	lookup: function lookup(cell, formatterParams, onRendered) {
 		var value = cell.getValue();
