b929e30d2c2da6c3a7adb9ce821c0c16eeb01464
Oli Folkerd <oliver.folkerd@gmail.com>
2018-10-27 11:02:25 +0100

    include updated dist for last property change

--- dist/js/tabulator.js
+++ dist/js/tabulator.js
@@ -10608,20 +10608,20 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 			this.expandEl.innerHTML = "<div class='tabulator-data-tree-control-expand'></div>";
 		}
 
-		switch (_typeof(options.dataTreeStartOpen)) {
+		switch (_typeof(options.dataTreeStartExpanded)) {
 			case "boolean":
 				this.startOpen = function (row, index) {
-					return options.dataTreeStartOpen;
+					return options.dataTreeStartExpanded;
 				};
 				break;
 
 			case "function":
-				this.startOpen = options.dataTreeStartOpen;
+				this.startOpen = options.dataTreeStartExpanded;
 				break;
 
 			default:
 				this.startOpen = function (row, index) {
-					return options.dataTreeStartOpen[index];
+					return options.dataTreeStartExpanded[index];
 				};
 				break;
 		}
