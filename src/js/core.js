1ede015aece418a1e284746ec586087d6bfc4185
James Compton <james.compton@specmo.com>
2018-11-01 09:33:16 -0400

    Remove Comments. Change clone date case to copy as New.

--- src/js/core.js
+++ src/js/core.js
@@ -1571,15 +1571,11 @@ Tabulator.prototype.helpers = {
 	},
 
     deepClone: function(obj){
-        // xxx deepClone is losing columns with a value of date.
-        // when calling this recursive, and the grid has a Date value, the grid loses the date value causing an empty object to be returned.
-        // So data is lost when calling getData() as a result.
-        // Add a test to see if obj is a date and allow it to pass through like any other value.  This fixes the data loss for this issue since Dates are 'objects'.
         var clone = {};
         for(var i in obj) {
             if (obj[i] != null && typeof(obj[i]) === "object") {
                 if (obj[i] instanceof Date) {
-                    clone[i] = obj[i];
+                    clone[i] = new Date(obj[i]);
                 } else {
                     clone[i] = this.deepClone(obj[i]);
                 }
