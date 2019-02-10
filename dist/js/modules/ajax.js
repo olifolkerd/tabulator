d6ef99825b08f2a40b2753d9f8105ca1f6543dfa
Oli Folkerd <oliver.folkerd@gmail.com>
2019-02-10 14:09:38 +0000

    only  set  Access-Control-Allow-Origin header automatically if not already defined by the user

--- dist/js/modules/ajax.js
+++ dist/js/modules/ajax.js
@@ -394,7 +394,10 @@ Ajax.prototype.defaultLoaderPromise = function (url, config, params) {
 			}
 
 			if (config.mode == "cors") {
-				config.headers["Access-Control-Allow-Origin"] = window.location.origin;
+
+				if (typeof config.headers["Access-Control-Allow-Origin"] === "undefined") {
+					config.headers["Access-Control-Allow-Origin"] = window.location.origin;
+				}
 
 				if (typeof config.credentials === "undefined") {
 					config.credentials = 'same-origin';
