0290bcd302637d61a4c81e2f675cd58778e8f31f
Oli Folkerd <oliver.folkerd@gmail.com>
2019-02-17 18:06:19 +0000

    update version number to 4.2.1

--- dist/js/modules/frozen_columns.js
+++ dist/js/modules/frozen_columns.js
@@ -1,4 +1,4 @@
-/* Tabulator v4.2.0 (c) Oliver Folkerd */
+/* Tabulator v4.2.1 (c) Oliver Folkerd */
 
 var FrozenColumns = function FrozenColumns(table) {
 	this.table = table; //hold Tabulator object
--- dist/js/modules/resize_table.js
+++ dist/js/modules/resize_table.js
@@ -1,4 +1,4 @@
-/* Tabulator v4.2.0 (c) Oliver Folkerd */
+/* Tabulator v4.2.1 (c) Oliver Folkerd */
 
 var ResizeTable = function ResizeTable(table) {
 	this.table = table; //hold Tabulator object
