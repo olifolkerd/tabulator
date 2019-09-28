ce6b70f5576a5d4570f74d8463715e3d25a41719
Oli Folkerd <oliver.folkerd@gmail.com>
2019-09-28 18:04:17 +0100

    update version number to 4.4.2

--- dist/js/modules/accessor.js
+++ dist/js/modules/accessor.js
@@ -1,6 +1,6 @@
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-/* Tabulator v4.4.1 (c) Oliver Folkerd */
+/* Tabulator v4.4.2 (c) Oliver Folkerd */
 
 var Accessor = function Accessor(table) {
 	this.table = table; //hold Tabulator object
