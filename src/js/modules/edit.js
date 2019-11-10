9233bb2c686ac7a7922b9f2bf7f39c36d12b2e79
Oli Folkerd <oliver.folkerd@gmail.com>
2019-11-10 09:58:57 +0000

    add verticalNavigation to editorParams for select editor

--- src/js/modules/edit.js
+++ src/js/modules/edit.js
@@ -635,6 +635,7 @@ Edit.prototype.editors = {
 		var self = this,
 		cellEl = cell.getElement(),
 		initialValue = cell.getValue(),
+		vertNav = editorParams.verticalNavigation || "editor",
 		initialDisplayValue = typeof initialValue !== "undefined" || initialValue === null ? initialValue : (typeof editorParams.defaultValue !== "undefined" ? editorParams.defaultValue : ""),
 		input = document.createElement("input"),
 		listEl = document.createElement("div"),
@@ -922,29 +923,33 @@ Edit.prototype.editors = {
 
 			switch(e.keyCode){
 				case 38: //up arrow
-				e.stopImmediatePropagation();
-				e.stopPropagation();
-				e.preventDefault();
-
 				index = dataItems.indexOf(currentItem);
 
-				if(index > 0){
-					setCurrentItem(dataItems[index - 1]);
+				if(vertNav == "editor" || (vertNav == "hybrid" && index)){
+					e.stopImmediatePropagation();
+					e.stopPropagation();
+					e.preventDefault();
+
+					if(index > 0){
+						setCurrentItem(dataItems[index - 1]);
+					}
 				}
 				break;
 
 				case 40: //down arrow
-				e.stopImmediatePropagation();
-				e.stopPropagation();
-				e.preventDefault();
-
 				index = dataItems.indexOf(currentItem);
 
-				if(index < dataItems.length - 1){
-					if(index == -1){
-						setCurrentItem(dataItems[0]);
-					}else{
-						setCurrentItem(dataItems[index + 1]);
+				if(vertNav == "editor" || (vertNav == "hybrid" && index < dataItems.length - 1)){
+					e.stopImmediatePropagation();
+					e.stopPropagation();
+					e.preventDefault();
+
+					if(index < dataItems.length - 1){
+						if(index == -1){
+							setCurrentItem(dataItems[0]);
+						}else{
+							setCurrentItem(dataItems[index + 1]);
+						}
 					}
 				}
 				break;
