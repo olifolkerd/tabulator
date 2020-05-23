eb5dd5dc9cdcd101f72fd984f0e8bd2d19e0e0c4
Oli Folkerd <oliver.folkerd@gmail.com>
2020-05-23 10:48:22 +0100

    add pollyfills for array.includes and object.assign

--- dist/js/tabulator.js
+++ dist/js/tabulator.js
@@ -176,6 +176,151 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		});
 	}
 
+	// https://tc39.github.io/ecma262/#sec-array.prototype.includes
+
+
+	if (!Array.prototype.includes) {
+
+		Object.defineProperty(Array.prototype, 'includes', {
+
+			value: function value(searchElement, fromIndex) {
+
+				if (this == null) {
+
+					throw new TypeError('"this" is null or not defined');
+				}
+
+				// 1. Let O be ? ToObject(this value).
+
+
+				var o = Object(this);
+
+				// 2. Let len be ? ToLength(? Get(O, "length")).
+
+
+				var len = o.length >>> 0;
+
+				// 3. If len is 0, return false.
+
+
+				if (len === 0) {
+
+					return false;
+				}
+
+				// 4. Let n be ? ToInteger(fromIndex).
+
+
+				//    (If fromIndex is undefined, this step produces the value 0.)
+
+
+				var n = fromIndex | 0;
+
+				// 5. If n ≥ 0, then
+
+
+				//  a. Let k be n.
+
+
+				// 6. Else n < 0,
+
+
+				//  a. Let k be len + n.
+
+
+				//  b. If k < 0, let k be 0.
+
+
+				var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+				function sameValueZero(x, y) {
+
+					return x === y || typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y);
+				}
+
+				// 7. Repeat, while k < len
+
+
+				while (k < len) {
+
+					// a. Let elementK be the result of ? Get(O, ! ToString(k)).
+
+
+					// b. If SameValueZero(searchElement, elementK) is true, return true.
+
+
+					if (sameValueZero(o[k], searchElement)) {
+
+						return true;
+					}
+
+					// c. Increase k by 1.
+
+
+					k++;
+				}
+
+				// 8. Return false
+
+
+				return false;
+			}
+
+		});
+	}
+
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+
+
+	if (typeof Object.assign !== 'function') {
+
+		// Must be writable: true, enumerable: false, configurable: true
+
+
+		Object.defineProperty(Object, "assign", {
+
+			value: function assign(target, varArgs) {
+				// .length of function is 2
+
+
+				'use strict';
+
+				if (target === null || target === undefined) {
+
+					throw new TypeError('Cannot convert undefined or null to object');
+				}
+
+				var to = Object(target);
+
+				for (var index = 1; index < arguments.length; index++) {
+
+					var nextSource = arguments[index];
+
+					if (nextSource !== null && nextSource !== undefined) {
+
+						for (var nextKey in nextSource) {
+
+							// Avoid bugs when hasOwnProperty is shadowed
+
+
+							if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+
+								to[nextKey] = nextSource[nextKey];
+							}
+						}
+					}
+				}
+
+				return to;
+			},
+
+			writable: true,
+
+			configurable: true
+
+		});
+	}
+
 	var ColumnManager = function ColumnManager(table) {
 
 		this.table = table; //hold parent table
@@ -7877,9 +8022,6 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		movableRowsReceivedFailed: function movableRowsReceivedFailed() {},
 
 		movableRowsReceivingStop: function movableRowsReceivingStop() {},
-
-		menuParentElement: document.body, //parent element for menu
-
 
 		scrollToRowPosition: "top",
 
