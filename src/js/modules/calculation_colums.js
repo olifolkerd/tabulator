cd08d46020998e7ffae215e94dc9f8a1319eafa3
Oli Folkerd <oliver.folkerd@gmail.com>
2020-09-13 11:47:49 +0100

    fix typo in CalcComponent definition

--- src/js/modules/calculation_colums.js
+++ src/js/modules/calculation_colums.js
@@ -11,7 +11,7 @@ CalcComponent.prototype.getElement = function(){
 	return this._row.getElement();
 };
 
-RowComponent.prototype.getTable = function(){
+CalcComponent.prototype.getTable = function(){
 	return this._row.table;
 };
 
