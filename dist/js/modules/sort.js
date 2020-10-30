3c29a60b217cbe2f9609a62de9a54b7445c2e375
Oli Folkerd <oliver.folkerd@gmail.com>
2020-10-30 08:40:55 +0000

    update version number to 4.8.4

--- dist/js/modules/sort.js
+++ dist/js/modules/sort.js
@@ -1,6 +1,6 @@
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-/* Tabulator v4.8.3 (c) Oliver Folkerd */
+/* Tabulator v4.8.4 (c) Oliver Folkerd */
 
 var Sort = function Sort(table) {
 	this.table = table; //hold Tabulator object
