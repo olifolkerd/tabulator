65f287b13548a96ac7061b02967593f38750c2cc
Oli Folkerd <oliver.folkerd@gmail.com>
2020-11-22 16:19:18 +0000

    update version number to 4.9.0

--- dist/js/modules/resize_columns.js
+++ dist/js/modules/resize_columns.js
@@ -1,4 +1,4 @@
-/* Tabulator v4.8.4 (c) Oliver Folkerd */
+/* Tabulator v4.9.0 (c) Oliver Folkerd */
 
 var ResizeColumns = function ResizeColumns(table) {
 	this.table = table; //hold Tabulator object
