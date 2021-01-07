02347b3775f84d50b82c178d12b0e1535a1dca0a
Oli Folkered <oli@event.engineering>
2021-01-07 20:29:11 +0000

    map keybindings module to new directory format

---
+++ src/js/modules/Keybindings/defaults/bindings.js
@@ -0,0 +1,13 @@
+module.exports = {
+	navPrev:"shift + 9",
+	navNext:9,
+	navUp:38,
+	navDown:40,
+	scrollPageUp:33,
+	scrollPageDown:34,
+	scrollToStart:36,
+	scrollToEnd:35,
+	undo:"ctrl + 90",
+	redo:"ctrl + 89",
+	copyToClipboard:"ctrl + 67",
+};
