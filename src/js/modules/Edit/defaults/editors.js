41f2b25e1a9dbaa702855152cc92dded6ded29f8
Oli Folkered <oli@event.engineering>
2021-01-07 18:52:16 +0000

    map edit module to new directory format

---
+++ src/js/modules/Edit/defaults/editors.js
@@ -0,0 +1,21 @@
+import input from './downloaders/input.js';
+import textarea from './downloaders/textarea.js';
+import number from './downloaders/number.js';
+import range from './downloaders/range.js';
+import select from './downloaders/select.js';
+import autocomplete from './downloaders/autocomplete.js';
+import star from './downloaders/star.js';
+import progress from './downloaders/progress.js';
+import tickCross from './downloaders/tickCross.js';
+
+module.exports = {
+	input:input,
+	textarea:textarea,
+    number:number,
+    range:range,
+	select:select,
+	autocomplete:autocomplete,
+	star:star,
+	progress:progress,
+	tickCross:tickCross,
+};
---
+++ src/js/modules/Edit/defaults/editors/input.js
@@ -0,0 +1,69 @@
+//input element
+module.exports = function(cell, onRendered, success, cancel, editorParams){
+	//create and style input
+	var cellValue = cell.getValue(),
+	input = document.createElement("input");
+
+	input.setAttribute("type", editorParams.search ? "search" : "text");
+
+	input.style.padding = "4px";
+	input.style.width = "100%";
+	input.style.boxSizing = "border-box";
+
+	if(editorParams.elementAttributes && typeof editorParams.elementAttributes == "object"){
+		for (let key in editorParams.elementAttributes){
+			if(key.charAt(0) == "+"){
+				key = key.slice(1);
+				input.setAttribute(key, input.getAttribute(key) + editorParams.elementAttributes["+" + key]);
+			}else{
+				input.setAttribute(key, editorParams.elementAttributes[key]);
+			}
+		}
+	}
+
+	input.value = typeof cellValue !== "undefined" ? cellValue : "";
+
+	onRendered(function(){
+		input.focus({preventScroll: true});
+		input.style.height = "100%";
+	});
+
+	function onChange(e){
+		if(((cellValue === null || typeof cellValue === "undefined") && input.value !== "") || input.value !== cellValue){
+			if(success(input.value)){
+				cellValue = input.value; //persist value if successfully validated incase editor is used as header filter
+			}
+		}else{
+			cancel();
+		}
+	}
+
+	//submit new value on blur or change
+	input.addEventListener("change", onChange);
+	input.addEventListener("blur", onChange);
+
+	//submit new value on enter
+	input.addEventListener("keydown", function(e){
+		switch(e.keyCode){
+			// case 9:
+			case 13:
+			onChange(e);
+			break;
+
+			case 27:
+			cancel();
+			break;
+
+			case 35:
+			case 36:
+			e.stopPropagation();
+			break;
+		}
+	});
+
+	if(editorParams.mask){
+		this.table.modules.edit.maskInput(input, editorParams);
+	}
+
+	return input;
+};
