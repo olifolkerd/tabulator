587626ce94fb31e9e855705dcfd444d39740da3f
Oli Folkered <oli@event.engineering>
2021-01-10 19:41:10 +0000

    tweak rollup config

--- build/rollup.js
+++ build/rollup.js
@@ -4,6 +4,34 @@ import babel from "@rollup/plugin-babel";
 import scss  from "rollup-plugin-scss";
 import del  from "rollup-plugin-delete";
 
+const version_no = "4.9.3";
+const version = "/* Tabulator v" + version_no + " (c) Oliver Folkerd */\n";
+
+
+
+// const globby = require('globby');
+// ./src/scss/**/tabulator*.scss
+// const configs = globby.sync('**/*.js').map(inputFile => ({
+//   entry: inputFile
+//   dest: inputFile.replace('src', 'dist'),
+//   format: 'umd'
+// }));
+// module.exports = configs
+//
+// const input = ["src/js/esm.js"];
+// export default [
+// {
+//   input: './src/scss/tabulator.scss',
+//   output: {
+//         file: './dist/css/tabulator.css',
+//       },
+//   plugins: [
+//     del({ targets: 'dist/*' }),
+//     scss ({output:false}),
+//   ],
+// },
+
+
 const input = ["src/js/esm.js"];
 export default [
 {
@@ -24,42 +52,52 @@ export default [
 //     }),
 //   ],
 // },
-  {
-    // UMD
-    input:"src/js/tabulator_full.js",
-    plugins: [
-      nodeResolve(),
-      babel({
-        babelHelpers: "bundled",
-      }),
-      terser(),
-    ],
-    output: {
-      file: `dist/js/tabulator.js`,
-      format: "umd",
-      name: "Tabulator", // this is the name of the global object
-      esModule: false,
-      exports: "default",
-      sourcemap: true,
-    },
-  },
+  // {
+  //   // UMD
+  //   input:"src/js/tabulator_full.js",
+  //   plugins: [
+  //     nodeResolve(),
+  //     babel({
+  //       babelHelpers: "bundled",
+  //     }),
+  //     terser(),
+  //   ],
+  //   output: {
+  //     file: `dist/js/tabulator.js`,
+  //     format: "umd",
+  //     name: "Tabulator", // this is the name of the global object
+  //     esModule: false,
+  //     exports: "default",
+  //     sourcemap: true,
+  //     banner: version,
+  //   },
+  // },
 // ESM and CJS
   {
     input,
     plugins: [nodeResolve()],
     output: [
       {
-        dir: "dist/js/esm",
+        file: "dist/js/esm.js",
         format: "esm",
         exports: "named",
         sourcemap: true,
+        plugins:[terser({
+          format:{
+            comments:function(node, comment){
+              console.log("version", version)
+              return comment.value.startsWith("/* Tabulator");
+            }
+          }
+        })],
+        banner: version,
       },
-      {
-        dir: "dist/js/cjs",
-        format: "cjs",
-        exports: "named",
-        sourcemap: true,
-      },
+      // {
+      //   dir: "dist/js/cjs",
+      //   format: "cjs",
+      //   exports: "named",
+      //   sourcemap: true,
+      // },
     ],
   },
 ];
