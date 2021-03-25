ce8bd2c5e11641480af616ab2a5983482bb76b6b
phaleth <james62@email.cz>
2021-03-25 20:08:16 +0100

    Ajax module IE11 compatibility fix

--- src/js/modules/Ajax/Ajax.js
+++ src/js/modules/Ajax/Ajax.js
@@ -37,7 +37,7 @@ class Ajax extends Module{
 			if(typeof this.table.options.ajaxLoaderLoading == "string"){
 				template = document.createElement('template');
 				template.innerHTML = this.table.options.ajaxLoaderLoading.trim();
-				this.loadingElement = template.content.firstChild;
+				this.loadingElement = template.firstElementChild;
 			}else{
 				this.loadingElement = this.table.options.ajaxLoaderLoading;
 			}
@@ -51,7 +51,7 @@ class Ajax extends Module{
 			if(typeof this.table.options.ajaxLoaderError == "string"){
 				template = document.createElement('template');
 				template.innerHTML = this.table.options.ajaxLoaderError.trim();
-				this.errorElement = template.content.firstChild;
+				this.errorElement = template.firstElementChild;
 			}else{
 				this.errorElement = this.table.options.ajaxLoaderError;
 			}
