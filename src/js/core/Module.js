00adb972650f3bf3641ed28bff83fbdd78b71eee
Oli Folkerd <oliver.folkerd@gmail.com>
2021-03-28 14:35:45 +0100

    add event bus internal subscription helper funcctions

--- src/js/core/Module.js
+++ src/js/core/Module.js
@@ -7,6 +7,14 @@ class Module{
 	initialize(){
 		// setup module when table is initialized, to be overriden in module
 	}
+
+	subscribe(){
+		this.table.eventBus.subscribe(...arguments);
+	}
+
+	unsubscribe(){
+		this.table.eventBus.unsubscribe(...arguments);
+	}
 }
 
 export default Module;
