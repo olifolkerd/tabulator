88f233b0e8f067e60f2023818f6b4f49696f83df
Oli Folkerd <oliver.folkerd@gmail.com>
2021-07-25 12:44:55 +0100

    change datetime sorter to use luxon

--- src/js/modules/Sort/defaults/sorters/datetime.js
+++ src/js/modules/Sort/defaults/sorters/datetime.js
@@ -1,16 +1,16 @@
 //sort datetime
 export default function(a, b, aRow, bRow, column, dir, params){
-	var format = params.format || "DD/MM/YYYY HH:mm:ss",
+	var DT = window.DateTime || luxon.DateTime;
+	var format = params.format || "dd/MM/yyyy HH:mm:ss",
 	alignEmptyValues = params.alignEmptyValues,
 	emptyAlign = 0;
 
-	if(typeof moment != "undefined"){
-		a = moment(a, format);
-		b = moment(b, format);
-
-		if(!a.isValid()){
-			emptyAlign = !b.isValid() ? 0 : -1;
-		}else if(!b.isValid()){
+	if(typeof DT != "undefined"){
+		a = DT.fromFormat(a, format);
+		b = DT.fromFormat(b, format);
+		if(!a.isValid){
+			emptyAlign = !b.isValid ? 0 : -1;
+		}else if(!b.isValid){
 			emptyAlign =  1;
 		}else{
 			//compare valid values
