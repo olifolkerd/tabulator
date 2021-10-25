0a6a4efae4ff93074bfed1a18b7a9c455df456cb
Oli Folkerd <oliver.folkerd@gmail.com>
2021-10-25 19:51:29 +0100

    improve formatting of negative numbers in money formatter

--- dist/js/tabulator_esm.js
+++ dist/js/tabulator_esm.js
@@ -8995,16 +8995,23 @@ function textarea$1(cell, formatterParams, onRendered){
 
 function money(cell, formatterParams, onRendered){
 	var floatVal = parseFloat(cell.getValue()),
+	sign = "",
 	number, integer, decimal, rgx;
 
 	var decimalSym = formatterParams.decimal || ".";
 	var thousandSym = formatterParams.thousand || ",";
+	var negativeSign = formatterParams.negativeSign || "-";
 	var symbol = formatterParams.symbol || "";
 	var after = !!formatterParams.symbolAfter;
 	var precision = typeof formatterParams.precision !== "undefined" ? formatterParams.precision : 2;
 
 	if(isNaN(floatVal)){
 		return this.emptyToSpace(this.sanitizeHTML(cell.getValue()));
+	}
+
+	if(floatVal < 0){
+		floatVal = Math.abs(floatVal);
+		sign = negativeSign;
 	}
 
 	number = precision !== false ? floatVal.toFixed(precision) : floatVal;
@@ -9019,7 +9026,7 @@ function money(cell, formatterParams, onRendered){
 		integer = integer.replace(rgx, "$1" + thousandSym + "$2");
 	}
 
-	return after ? integer + decimal + symbol : symbol + integer + decimal;
+	return after ? sign + integer + decimal + symbol : sign + symbol + integer + decimal;
 }
 
 function link(cell, formatterParams, onRendered){
--- src/js/modules/Format/defaults/formatters/money.js
+++ src/js/modules/Format/defaults/formatters/money.js
@@ -1,15 +1,22 @@
 export default function(cell, formatterParams, onRendered){
 	var floatVal = parseFloat(cell.getValue()),
+	sign = "",
 	number, integer, decimal, rgx;
 
 	var decimalSym = formatterParams.decimal || ".";
 	var thousandSym = formatterParams.thousand || ",";
+	var negativeSign = formatterParams.negativeSign || "-";
 	var symbol = formatterParams.symbol || "";
 	var after = !!formatterParams.symbolAfter;
 	var precision = typeof formatterParams.precision !== "undefined" ? formatterParams.precision : 2;
 
 	if(isNaN(floatVal)){
 		return this.emptyToSpace(this.sanitizeHTML(cell.getValue()));
+	}
+
+	if(floatVal < 0){
+		floatVal = Math.abs(floatVal);
+		sign = negativeSign;
 	}
 
 	number = precision !== false ? floatVal.toFixed(precision) : floatVal;
@@ -24,5 +31,5 @@ export default function(cell, formatterParams, onRendered){
 		integer = integer.replace(rgx, "$1" + thousandSym + "$2");
 	}
 
-	return after ? integer + decimal + symbol : symbol + integer + decimal;
+	return after ? sign + integer + decimal + symbol : sign + symbol + integer + decimal;
 };
