bafe8948b44c426a5d5cee8efe9d1baa19f88be9
Matthew Rathbone <matthew.rathbone@gmail.com>
2022-03-11 10:50:22 -0600

    eslint rule for tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-			"no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }]
-    }
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": ["error", "tab"],
+		"no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }]
+	}
 }
