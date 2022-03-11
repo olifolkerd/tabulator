32eeaecdb6fc31b6473161980dd0dbfce875b47a
Matthew Rathbone <matthew.rathbone@gmail.com>
2022-03-11 10:36:19 -0600

    initial eslint work

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+			"no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }]
+    }
+}
