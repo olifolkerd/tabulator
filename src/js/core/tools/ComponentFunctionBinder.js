0de9e9288ea2ad587d7adfbe56e8e62126043213
Pavel <16826622+Pashted@users.noreply.github.com>
2022-04-02 18:23:42 +0300

    Fix `TypeError: this.bindings[type][name].bind is not a function` when running under VueJs3.2 behavior. (#3659)
    Fix typos.

--- src/js/core/tools/ComponentFunctionBinder.js
+++ src/js/core/tools/ComponentFunctionBinder.js
@@ -12,14 +12,14 @@ export default class ComponentFunctionBinder{
 		}
 
 		if(this.bindings[type][funcName]){
-			console.warn("Unable to bind component handler, a matching function name is already bound", type, funcName, hanlder)
+			console.warn("Unable to bind component handler, a matching function name is already bound", type, funcName, handler)
 		}else{
 			this.bindings[type][funcName] = handler;
 		}
 	}
 
 	handle(type, component, name){
-		if(this.bindings[type] && this.bindings[type][name]){
+		if(this.bindings[type] && this.bindings[type][name] && typeof this.bindings[type][name].bind === 'function'){
 			return this.bindings[type][name].bind(null, component);
 		}else{
 			if(name !== "then" && typeof name === "string" && !name.startsWith("_")){
