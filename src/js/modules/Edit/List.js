8318f4d1d603b24a241af8e9074aa9304683d0f5
Oli Folkerd <oliver.folkerd@gmail.com>
2022-06-19 17:54:00 +0100

    fix comparison function issue in list filter func

--- src/js/modules/Edit/List.js
+++ src/js/modules/Edit/List.js
@@ -764,7 +764,7 @@ export default class Edit{
 		term = String(term).toLowerCase();
         
 		if(label !== null || typeof label !== "undefined"){
-			if(String(label).toLowerCase().indexOf(term) > -1 || String(value).toLowerCase(term).indexOf() > -1){
+			if(String(label).toLowerCase().indexOf(term) > -1 || String(value).toLowerCase().indexOf(term) > -1){
 				return true;
 			}
 		}
