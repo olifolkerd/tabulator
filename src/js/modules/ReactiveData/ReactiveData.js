3e7bc7ff008eebdc486d8c286f70e62049b7bd8b
Oli Folkerd <oliver.folkerd@gmail.com>
2022-06-19 17:35:39 +0100

    fix scope issue in reactive data module

--- src/js/modules/ReactiveData/ReactiveData.js
+++ src/js/modules/ReactiveData/ReactiveData.js
@@ -1,18 +1,18 @@
 import Module from '../../core/Module.js';
 
 class ReactiveData extends Module{
-
+	
 	constructor(table){
 		super(table);
-
+		
 		this.data = false;
 		this.blocked = false; //block reactivity while performing update
 		this.origFuncs = {}; // hold original data array functions to allow replacement after data is done with
 		this.currentVersion = 0;
-
+		
 		this.registerTableOption("reactiveData", false); //enable data reactivity
 	}
-
+	
 	initialize(){
 		if(this.table.options.reactiveData){
 			this.subscribe("cell-value-save-before", this.block.bind(this));
@@ -24,84 +24,84 @@ class ReactiveData extends Module{
 			this.subscribe("table-destroy", this.unwatchData.bind(this));
 		}
 	}
-
+	
 	watchData(data){
 		var self = this,
 		version;
-
+		
 		this.currentVersion ++;
-
+		
 		version = this.currentVersion;
-
+		
 		this.unwatchData();
-
+		
 		this.data = data;
-
+		
 		//override array push function
 		this.origFuncs.push = data.push;
-
+		
 		Object.defineProperty(this.data, "push", {
 			enumerable: false,
 			configurable: true,
 			value: function(){
 				var args = Array.from(arguments);
-
+				
 				if(!self.blocked && version === self.currentVersion){
 					args.forEach((arg) => {
 						self.table.rowManager.addRowActual(arg, false);
 					});
 				}
-
+				
 				return self.origFuncs.push.apply(data, arguments);
 			}
 		});
-
+		
 		//override array unshift function
 		this.origFuncs.unshift = data.unshift;
-
+		
 		Object.defineProperty(this.data, "unshift", {
 			enumerable: false,
 			configurable: true,
 			value: function(){
 				var args = Array.from(arguments);
-
+				
 				if(!self.blocked && version === self.currentVersion){
 					args.forEach((arg) => {
 						self.table.rowManager.addRowActual(arg, true);
 					});
 				}
-
+				
 				return self.origFuncs.unshift.apply(data, arguments);
 			}
 		});
-
-
+		
+		
 		//override array shift function
 		this.origFuncs.shift = data.shift;
-
+		
 		Object.defineProperty(this.data, "shift", {
 			enumerable: false,
 			configurable: true,
 			value: function(){
 				var row;
-
+				
 				if(!self.blocked && version === self.currentVersion){
 					if(self.data.length){
 						row = self.table.rowManager.getRowFromDataObject(self.data[0]);
-
+						
 						if(row){
 							row.deleteActual();
 						}
 					}
 				}
-
+				
 				return self.origFuncs.shift.call(data);
 			}
 		});
-
+		
 		//override array pop function
 		this.origFuncs.pop = data.pop;
-
+		
 		Object.defineProperty(this.data, "pop", {
 			enumerable: false,
 			configurable: true,
@@ -110,7 +110,7 @@ class ReactiveData extends Module{
 				if(!self.blocked && version === self.currentVersion){
 					if(self.data.length){
 						row = self.table.rowManager.getRowFromDataObject(self.data[self.data.length - 1]);
-
+						
 						if(row){
 							row.deleteActual();
 						}
@@ -119,11 +119,11 @@ class ReactiveData extends Module{
 				return self.origFuncs.pop.call(data);
 			}
 		});
-
-
+		
+		
 		//override array splice function
 		this.origFuncs.splice = data.splice;
-
+		
 		Object.defineProperty(this.data, "splice", {
 			enumerable: false,
 			configurable: true,
@@ -133,49 +133,49 @@ class ReactiveData extends Module{
 				end = args[1],
 				newRows = args[2] ? args.slice(2) : false,
 				startRow;
-
+				
 				if(!self.blocked && version === self.currentVersion){
-
+					
 					//add new rows
 					if(newRows){
 						startRow = data[start] ? self.table.rowManager.getRowFromDataObject(data[start]) : false;
-
+						
 						if(startRow){
 							newRows.forEach((rowData) => {
 								self.table.rowManager.addRowActual(rowData, true, startRow, true);
 							});
 						}else{
 							newRows = newRows.slice().reverse();
-
+							
 							newRows.forEach((rowData) => {
 								self.table.rowManager.addRowActual(rowData, true, false, true);
 							});
 						}
 					}
-
+					
 					//delete removed rows
 					if(end !== 0){
 						var oldRows = data.slice(start, typeof args[1] === "undefined" ? args[1] : start + end);
-
+						
 						oldRows.forEach((rowData, i) => {
 							var row = self.table.rowManager.getRowFromDataObject(rowData);
-
+							
 							if(row){
 								row.deleteActual(i !== oldRows.length - 1);
 							}
 						});
 					}
-
+					
 					if(newRows || end !== 0){
 						self.table.rowManager.reRenderInPosition();
 					}
 				}
-
+				
 				return self.origFuncs.splice.apply(data, arguments);
 			}
 		});
 	}
-
+	
 	unwatchData(){
 		if(this.data !== false){
 			for(var key in this.origFuncs){
@@ -188,112 +188,112 @@ class ReactiveData extends Module{
 			}
 		}
 	}
-
+	
 	watchRow(row){
 		var data = row.getData();
-
+		
 		this.blocked = true;
-
+		
 		for(var key in data){
 			this.watchKey(row, data, key);
 		}
-
+		
 		if(this.table.options.dataTree){
 			this.watchTreeChildren(row);
 		}
-
+		
 		this.blocked = false;
 	}
-
+	
 	watchTreeChildren (row){
 		var childField = row.getData()[this.table.options.dataTreeChildField],
 		origFuncs = {};
-
-		function rebuildTree(){
-			this.table.modules.dataTree.initializeRow(row);
-			this.table.modules.dataTree.layoutRow(row);
-			this.table.rowManager.refreshActiveData("tree", false, true);
-		}
-
+		
 		if(childField){
-
+			
 			origFuncs.push = childField.push;
-
+			
 			Object.defineProperty(childField, "push", {
 				enumerable: false,
 				configurable: true,
 				value: () => {
 					var result = origFuncs.push.apply(childField, arguments);
-
-					rebuildTree();
-
+					
+					this.rebuildTree(row);
+					
 					return result;
 				}
 			});
-
+			
 			origFuncs.unshift = childField.unshift;
-
+			
 			Object.defineProperty(childField, "unshift", {
 				enumerable: false,
 				configurable: true,
 				value: () => {
 					var result =  origFuncs.unshift.apply(childField, arguments);
-
-					rebuildTree();
-
+					
+					this.rebuildTree(row);
+					
 					return result;
 				}
 			});
-
+			
 			origFuncs.shift = childField.shift;
-
+			
 			Object.defineProperty(childField, "shift", {
 				enumerable: false,
 				configurable: true,
 				value: () => {
 					var result =  origFuncs.shift.call(childField);
-
-					rebuildTree();
-
+					
+					this.rebuildTree(row);
+					
 					return result;
 				}
 			});
-
+			
 			origFuncs.pop = childField.pop;
-
+			
 			Object.defineProperty(childField, "pop", {
 				enumerable: false,
 				configurable: true,
 				value: () => {
 					var result =  origFuncs.pop.call(childField);
-
-					rebuildTree();
-
+					
+					this.rebuildTree(row);
+					
 					return result;
 				}
 			});
-
+			
 			origFuncs.splice = childField.splice;
-
+			
 			Object.defineProperty(childField, "splice", {
 				enumerable: false,
 				configurable: true,
 				value: () => {
 					var result =  origFuncs.splice.apply(childField, arguments);
-
-					rebuildTree();
-
+					
+					this.rebuildTree(row);
+					
 					return result;
 				}
 			});
 		}
 	}
-
+	
+	rebuildTree(row){
+		this.table.modules.dataTree.initializeRow(row);
+		this.table.modules.dataTree.layoutRow(row);
+		this.table.rowManager.refreshActiveData("tree", false, true);
+	}
+	
 	watchKey(row, data, key){
 		var props = Object.getOwnPropertyDescriptor(data, key),
 		value = data[key],
 		version = this.currentVersion;
-
+		
 		Object.defineProperty(data, key, {
 			set: (newValue) => {
 				value = newValue;
@@ -302,36 +302,36 @@ class ReactiveData extends Module{
 					update[key] = newValue;
 					row.updateData(update);
 				}
-
+				
 				if(props.set){
 					props.set(newValue);
 				}
 			},
 			get:() => {
-
+				
 				if(props.get){
 					props.get();
 				}
-
+				
 				return value;
 			}
 		});
 	}
-
+	
 	unwatchRow(row){
 		var data = row.getData();
-
+		
 		for(var key in data){
 			Object.defineProperty(data, key, {
 				value:data[key],
 			});
 		}
 	}
-
+	
 	block(){
 		this.blocked = true;
 	}
-
+	
 	unblock(){
 		this.blocked = false;
 	}
