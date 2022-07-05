546dc4648290360a15ccb165f36c7b7ec6b3e8df
Oli Folkerd <oliver.folkerd@gmail.com>
2022-07-05 07:25:54 +0100

    add date editor

--- dist/js/tabulator_esm.js
+++ dist/js/tabulator_esm.js
@@ -5562,6 +5562,105 @@ function range(cell, onRendered, success, cancel, editorParams){
 	return input;
 }
 
+//input element
+function date(cell, onRendered, success, cancel, editorParams){
+	var inputFormat = editorParams.inputFormat,
+	DT = inputFormat ? (window.DateTime || luxon.DateTime) : null, 
+	newDatetime;
+
+	//create and style input
+	var cellValue = cell.getValue(),
+	input = document.createElement("input");
+	
+	input.type = "date";
+	input.style.padding = "4px";
+	input.style.width = "100%";
+	input.style.boxSizing = "border-box";
+	
+	if(editorParams.elementAttributes && typeof editorParams.elementAttributes == "object"){
+		for (let key in editorParams.elementAttributes){
+			if(key.charAt(0) == "+"){
+				key = key.slice(1);
+				input.setAttribute(key, input.getAttribute(key) + editorParams.elementAttributes["+" + key]);
+			}else {
+				input.setAttribute(key, editorParams.elementAttributes[key]);
+			}
+		}
+	}
+	
+	cellValue = typeof cellValue !== "undefined" ? cellValue : "";
+	
+	if(inputFormat){
+		if(DT){
+			if(DT.isDateTime(cellValue)){
+				newDatetime = cellValue;
+			}else if(inputFormat === "iso"){
+				newDatetime = DT.fromISO(String(cellValue));
+			}else {
+				newDatetime = DT.fromFormat(String(cellValue), inputFormat);
+			}
+
+			cellValue = newDatetime.toFormat("yyyy-MM-dd");
+
+		}else {
+			console.error("Editor Error - 'date' editor 'inputFormat' param is dependant on luxon.js");
+		}
+	}
+
+	input.value = cellValue;
+	
+	onRendered(function(){
+		input.focus({preventScroll: true});
+		input.style.height = "100%";
+		
+		if(editorParams.selectContents){
+			input.select();
+		}
+	});
+	
+	function onChange(e){
+		var value = input.value;
+
+		if(((cellValue === null || typeof cellValue === "undefined") && value !== "") || value !== cellValue){
+
+			if(value && inputFormat){
+				value = DT.fromFormat(String(value), "yyyy-MM-dd").toFormat(inputFormat);
+			}
+
+			if(success(value)){
+				cellValue = input.value; //persist value if successfully validated incase editor is used as header filter
+			}
+		}else {
+			cancel();
+		}
+	}
+	
+	//submit new value on blur or change
+	input.addEventListener("change", onChange);
+	input.addEventListener("blur", onChange);
+	
+	//submit new value on enter
+	input.addEventListener("keydown", function(e){
+		switch(e.keyCode){
+			// case 9:
+			case 13:
+			onChange();
+			break;
+			
+			case 27:
+			cancel();
+			break;
+			
+			case 35:
+			case 36:
+			e.stopPropagation();
+			break;
+		}
+	});
+	
+	return input;
+}
+
 class Edit{
 	constructor(editor, cell, onRendered, success, cancel, editorParams){
 		this.edit = editor;
@@ -6996,6 +7095,7 @@ var defaultEditors = {
 	textarea:textarea,
 	number:number,
 	range:range,
+	date:date,
 	select:select,
 	list:list,
 	autocomplete:autocomplete,
@@ -18154,7 +18254,7 @@ function datetime$1(a, b, aRow, bRow, column, dir, params){
 }
 
 //sort date
-function date(a, b, aRow, bRow, column, dir, params){
+function date$1(a, b, aRow, bRow, column, dir, params){
 	if(!params.format){
 		params.format = "dd/MM/yyyy";
 	}
@@ -18290,7 +18390,7 @@ function alphanum(as, bs, aRow, bRow, column, dir, params){
 var defaultSorters = {
 	number:number$1,
 	string:string,
-	date:date,
+	date:date$1,
 	time:time,
 	datetime:datetime$1,
 	boolean:boolean,
--- src/js/modules/Edit/defaults/editors.js
+++ src/js/modules/Edit/defaults/editors.js
@@ -2,6 +2,7 @@ import input from './editors/input.js';
 import textarea from './editors/textarea.js';
 import number from './editors/number.js';
 import range from './editors/range.js';
+import date from './editors/date.js';
 import select from './editors/select.js';
 import list from './editors/list.js';
 import autocomplete from './editors/autocomplete.js';
@@ -14,6 +15,7 @@ export default {
 	textarea:textarea,
 	number:number,
 	range:range,
+	date:date,
 	select:select,
 	list:list,
 	autocomplete:autocomplete,
