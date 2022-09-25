7e99d91f178ab972a7c7298db7bd267a18cc3750
Oli Folkerd <oliver.folkerd@gmail.com>
2022-09-25 15:33:56 +0100

    add missing getColumn and getTable functions to the mock cell component passed into the titleFormatter

--- dist/js/tabulator_esm.js
+++ dist/js/tabulator_esm.js
@@ -10422,13 +10422,13 @@ var defaultFormatters = {
 };
 
 class Format extends Module{
-
+	
 	constructor(table){
 		super(table);
-
+		
 		this.registerColumnOption("formatter");
 		this.registerColumnOption("formatterParams");
-
+		
 		this.registerColumnOption("formatterPrint");
 		this.registerColumnOption("formatterPrintParams");
 		this.registerColumnOption("formatterClipboard");
@@ -10438,136 +10438,142 @@ class Format extends Module{
 		this.registerColumnOption("titleFormatter");
 		this.registerColumnOption("titleFormatterParams");
 	}
-
+	
 	initialize(){
 		this.subscribe("cell-format", this.formatValue.bind(this));
 		this.subscribe("cell-rendered", this.cellRendered.bind(this));
 		this.subscribe("column-layout", this.initializeColumn.bind(this));
 		this.subscribe("column-format", this.formatHeader.bind(this));
 	}
-
+	
 	//initialize column formatter
 	initializeColumn(column){
 		column.modules.format = this.lookupFormatter(column, "");
-
+		
 		if(typeof column.definition.formatterPrint !== "undefined"){
 			column.modules.format.print = this.lookupFormatter(column, "Print");
 		}
-
+		
 		if(typeof column.definition.formatterClipboard !== "undefined"){
 			column.modules.format.clipboard = this.lookupFormatter(column, "Clipboard");
 		}
-
+		
 		if(typeof column.definition.formatterHtmlOutput !== "undefined"){
 			column.modules.format.htmlOutput = this.lookupFormatter(column, "HtmlOutput");
 		}
 	}
-
+	
 	lookupFormatter(column, type){
 		var config = {params:column.definition["formatter" + type + "Params"] || {}},
 		formatter = column.definition["formatter" + type];
-
+		
 		//set column formatter
 		switch(typeof formatter){
 			case "string":
-				if(Format.formatters[formatter]){
-					config.formatter = Format.formatters[formatter];
-				}else {
-					console.warn("Formatter Error - No such formatter found: ", formatter);
-					config.formatter = Format.formatters.plaintext;
-				}
-				break;
-
-			case "function":
-				config.formatter = formatter;
-				break;
-
-			default:
+			if(Format.formatters[formatter]){
+				config.formatter = Format.formatters[formatter];
+			}else {
+				console.warn("Formatter Error - No such formatter found: ", formatter);
 				config.formatter = Format.formatters.plaintext;
-				break;
+			}
+			break;
+			
+			case "function":
+			config.formatter = formatter;
+			break;
+			
+			default:
+			config.formatter = Format.formatters.plaintext;
+			break;
 		}
-
+		
 		return config;
 	}
-
+	
 	cellRendered(cell){
 		if(cell.modules.format && cell.modules.format.renderedCallback && !cell.modules.format.rendered){
 			cell.modules.format.renderedCallback();
 			cell.modules.format.rendered = true;
 		}
 	}
-
+	
 	//return a formatted value for a column header
 	formatHeader(column, title, el){
 		var formatter, params, onRendered, mockCell;
-
+		
 		if(column.definition.titleFormatter){
 			formatter = this.getFormatter(column.definition.titleFormatter);
-
+			
 			onRendered = (callback) => {
 				column.titleFormatterRendered = callback;
 			};
-
+			
 			mockCell = {
 				getValue:function(){
 					return title;
 				},
 				getElement:function(){
 					return el;
+				},
+				getColumn:function(){
+					return column.getComponent();
+				},
+				getTable:() => {
+					return this.table;
 				}
 			};
-
+			
 			params = column.definition.titleFormatterParams || {};
-
+			
 			params = typeof params === "function" ? params() : params;
-
+			
 			return formatter.call(this, mockCell, params, onRendered);
 		}else {
 			return title;
 		}
 	}
-
-
+	
+	
 	//return a formatted value for a cell
 	formatValue(cell){
 		var component = cell.getComponent(),
 		params = typeof cell.column.modules.format.params === "function" ? cell.column.modules.format.params(component) : cell.column.modules.format.params;
-
+		
 		function onRendered(callback){
 			if(!cell.modules.format){
 				cell.modules.format = {};
 			}
-
+			
 			cell.modules.format.renderedCallback = callback;
 			cell.modules.format.rendered = false;
 		}
-
+		
 		return cell.column.modules.format.formatter.call(this, component, params, onRendered);
 	}
-
+	
 	formatExportValue(cell, type){
 		var formatter = cell.column.modules.format[type],
 		params;
-
+		
 		if(formatter){
 			params = typeof formatter.params === "function" ? formatter.params(cell.getComponent()) : formatter.params;
-
+			
 			function onRendered(callback){
 				if(!cell.modules.format){
 					cell.modules.format = {};
 				}
-
+				
 				cell.modules.format.renderedCallback = callback;
 				cell.modules.format.rendered = false;
 			}
-
+			
 			return formatter.formatter.call(this, cell.getComponent(), params, onRendered);
-
+			
 		}else {
 			return this.formatValue(cell);
 		}
 	}
-
+	
 	sanitizeHTML(value){
 		if(value){
 			var entityMap = {
@@ -10580,7 +10586,7 @@ class Format extends Module{
 				'`': '&#x60;',
 				'=': '&#x3D;'
 			};
-
+			
 			return String(value).replace(/[&<>"'`=/]/g, function (s) {
 				return entityMap[s];
 			});
@@ -10588,32 +10594,32 @@ class Format extends Module{
 			return value;
 		}
 	}
-
+	
 	emptyToSpace(value){
 		return value === null || typeof value === "undefined" || value === "" ? "&nbsp;" : value;
 	}
-
+	
 	//get formatter for cell
 	getFormatter(formatter){
 		switch(typeof formatter){
 			case "string":
-				if(Format.formatters[formatter]){
-					formatter = Format.formatters[formatter];
-				}else {
-					console.warn("Formatter Error - No such formatter found: ", formatter);
-					formatter = Format.formatters.plaintext;
-				}
-				break;
-
-			case "function":
-				//Custom formatter Function, do nothing
-				break;
-
-			default:
+			if(Format.formatters[formatter]){
+				formatter = Format.formatters[formatter];
+			}else {
+				console.warn("Formatter Error - No such formatter found: ", formatter);
 				formatter = Format.formatters.plaintext;
-				break;
+			}
+			break;
+			
+			case "function":
+			//Custom formatter Function, do nothing
+			break;
+			
+			default:
+			formatter = Format.formatters.plaintext;
+			break;
 		}
-
+		
 		return formatter;
 	}
 }
