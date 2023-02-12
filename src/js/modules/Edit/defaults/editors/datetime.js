2f7eb4279813f4ede465b239c46d5f240583f1da
mastrdev <admin@mastr.dev>
2023-02-12 06:21:21 -0500

    Fix date/time editors (#4112)
    * Fix date/time editors
    
    The "change" event fires prematurely with keyboard entry, so it's better to use the "blur" event to process the final value. However, FireFox fires a "blur" event when the user opens the built-in date/time picker, so we have to add some logic to ignore that case.
    
    * Remove unused parameter 'e'

--- src/js/modules/Edit/defaults/editors/datetime.js
+++ src/js/modules/Edit/defaults/editors/datetime.js
@@ -53,7 +53,7 @@ export default function(cell, onRendered, success, cancel, editorParams){
 		}
 	});
 	
-	function onChange(e){
+	function onChange(){
 		var value = input.value;
 
 		if(((cellValue === null || typeof cellValue === "undefined") && value !== "") || value !== cellValue){
@@ -70,16 +70,19 @@ export default function(cell, onRendered, success, cancel, editorParams){
 		}
 	}
 	
-	//submit new value on blur or change
-	input.addEventListener("change", onChange);
-	input.addEventListener("blur", onChange);
+	//submit new value on blur
+	input.addEventListener("blur", function(e) {
+		if (e.relatedTarget || e.rangeParent || e.explicitOriginalTarget !== input) {
+			onChange(); // only on a "true" blur; not when focusing browser's date/time picker
+		}
+	});
 	
 	//submit new value on enter
 	input.addEventListener("keydown", function(e){
 		switch(e.keyCode){
 			// case 9:
 			case 13:
-				onChange(e);
+				onChange();
 				break;
 			
 			case 27:
