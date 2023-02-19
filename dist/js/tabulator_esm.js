34fd0e4f10596a822e1977901900b79061683061
Oli Folkerd <oliver.folkerd@gmail.com>
2023-02-19 11:17:25 +0000

    revert breaking PR for fitColumns Layout

--- dist/js/tabulator_esm.js
+++ dist/js/tabulator_esm.js
@@ -24437,7 +24437,7 @@ function fitDataStretch(columns, forced){
 
 //resize columns to fit
 function fitColumns(columns, forced){
-	var totalWidth = this.table.rowManager.element.clientWidth; //table element width
+	var totalWidth = this.table.rowManager.element.getBoundingClientRect().width; //table element width
 	var fixedWidth = 0; //total width of columns with a defined width
 	var flexWidth = 0; //total width available to flexible columns
 	var flexGrowUnits = 0; //total number of widthGrow blocks across all columns
