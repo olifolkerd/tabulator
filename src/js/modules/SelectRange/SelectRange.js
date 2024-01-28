effebd26e7cc729b68932e7f30d9d552b6bde3bf
Oli Folkerd <oliver.folkerd@gmail.com>
2024-01-28 14:04:14 +0000

    add ranges class to whole table while range in use

--- src/js/modules/SelectRange/SelectRange.js
+++ src/js/modules/SelectRange/SelectRange.js
@@ -65,6 +65,7 @@ class SelectRange extends Module {
 		
 		this.table.rowManager.element.appendChild(this.overlay);
 		this.table.columnManager.element.setAttribute("tabindex", 0);
+		this.table.element.classList.add("tabulator-ranges");
 		
 		if (this.table.modules.edit) {
 			this.table.modules.edit.elementToFocusOnBlur = this.table.rowManager.element;
@@ -309,6 +310,7 @@ class SelectRange extends Module {
 	///////////////////////////////////
 	
 	keyNavigate(dir, e){
+		console.log("nav");
 		if(this.navigate(false, false, dir)){
 			e.preventDefault();
 		}
@@ -350,17 +352,17 @@ class SelectRange extends Module {
 		if(jump){
 			switch(dir){
 				case "left":
-				nextCol = this.findJumpCellLeft(range.start.row, rangeEdge.col);
-				break;
+					nextCol = this.findJumpCellLeft(range.start.row, rangeEdge.col);
+					break;
 				case "right":
-				nextCol = this.findJumpCellRight(range.start.row, rangeEdge.col);
-				break;
+					nextCol = this.findJumpCellRight(range.start.row, rangeEdge.col);
+					break;
 				case "up":
-				nextRow = this.findJumpCellUp(rangeEdge.row, range.start.col);
-				break;
+					nextRow = this.findJumpCellUp(rangeEdge.row, range.start.col);
+					break;
 				case "down":
-				nextRow = this.findJumpCellDown(rangeEdge.row, range.start.col);
-				break;
+					nextRow = this.findJumpCellDown(rangeEdge.row, range.start.col);
+					break;
 			}
 		}else{
 			if(expand){
@@ -371,17 +373,17 @@ class SelectRange extends Module {
 			
 			switch(dir){
 				case "left":
-				nextCol = Math.max(nextCol - 1, 0);
-				break;
+					nextCol = Math.max(nextCol - 1, 0);
+					break;
 				case "right":
-				nextCol = Math.min(nextCol + 1, this.getColumns().length - 2);
-				break;
+					nextCol = Math.min(nextCol + 1, this.getColumns().length - 2);
+					break;
 				case "up":
-				nextRow = Math.max(nextRow - 1, 0);
-				break;
+					nextRow = Math.max(nextRow - 1, 0);
+					break;
 				case "down":
-				nextRow = Math.min(nextRow + 1, this.getRows().length - 1);
-				break;
+					nextRow = Math.min(nextRow + 1, this.getRows().length - 1);
+					break;
 			}
 		}
 		
@@ -551,7 +553,7 @@ class SelectRange extends Module {
 			this.addRange();
 			
 			this.beginSelection(element);
-			this.endSelection(element)
+			this.endSelection(element);
 		} else {
 			this.resetRanges();
 			
