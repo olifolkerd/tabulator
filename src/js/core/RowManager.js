65f502fb2912bdfb3ff30f59d2ac120109cc21da
Oli Folkerd <oliver.folkerd@gmail.com>
2024-02-11 18:01:22 +0000

    remove redundant console trace

--- src/js/core/RowManager.js
+++ src/js/core/RowManager.js
@@ -983,7 +983,6 @@ export default class RowManager extends CoreFeature{
 	}
 	
 	_showPlaceholder(){
-		console.trace("placeholder")
 		if(this.placeholder){
 			if(this.placeholder && this.placeholder.parentNode){
 				this.placeholder.parentNode.removeChild(this.placeholder);
