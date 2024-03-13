b1a223b418c93ec117f18d2385ecf724f754ed59
Oli Folkerd <oliver.folkerd@gmail.com>
2024-03-13 12:14:14 +0000

    export range component from library

--- src/js/builds/esm.js
+++ src/js/builds/esm.js
@@ -7,6 +7,7 @@ export {default as ColumnComponent} from '../core/column/ColumnComponent.js';
 export {default as RowComponent} from '../core/row/RowComponent.js';
 export {default as GroupComponent} from '../modules/GroupRows/GroupComponent.js';
 export {default as CalcComponent} from '../modules/ColumnCalcs/CalcComponent.js';
+export {default as RangeComponent} from '../modules/SelectRange/RangeComponent.js';
 
 export {default as PseudoRow} from '../core/row/PseudoRow.js';
 
