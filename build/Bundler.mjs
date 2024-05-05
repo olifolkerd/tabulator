4b29ed86cea34e843382d052cc3159a41692792d
Joshua Tahanlangit <contact@thisjt.me>
2024-05-05 19:57:59 +0800

    revert mjs back to js for bundler

--- build/Bundler.mjs
+++ build/Bundler.mjs
@@ -1,180 +1,181 @@
-import { createRequire } from 'node:module';
+import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 
-import license from 'rollup-plugin-license';
-import {globbySync} from 'globby';
-import fs from 'fs-extra';
+import license from "rollup-plugin-license";
+import { globbySync } from "globby";
+import fs from "fs-extra";
 
 import postcss from "rollup-plugin-postcss";
 
-export default class Bundler{
-	
-	constructor(version, env){
+export default class Bundler {
+	constructor(version, env) {
 		this.bundles = [];
-		
+
 		this.env = env;
-		this.version = "/* Tabulator v" + version + " (c) Oliver Folkerd <%= moment().format('YYYY') %> */";
+		this.version =
+			"/* Tabulator v" +
+			version +
+			" (c) Oliver Folkerd <%= moment().format('YYYY') %> */";
 	}
-	
-	_suppressUnnecessaryWarnings(warn, defaultHandler){
+
+	_suppressUnnecessaryWarnings(warn, defaultHandler) {
 		const ignoredCodes = {
-			"FILE_NAME_CONFLICT": true,
+			FILE_NAME_CONFLICT: true,
 		};
-		
-		var suppressed = false, 
-		codeHandler = ignoredCodes[warn.code];
-		
-		if(codeHandler){
-			suppressed = typeof codeHandler === "function" ? codeHandler(warn) : codeHandler;
+
+		var suppressed = false,
+			codeHandler = ignoredCodes[warn.code];
+
+		if (codeHandler) {
+			suppressed =
+				typeof codeHandler === "function" ? codeHandler(warn) : codeHandler;
 		}
-		
-		if(!suppressed){
+
+		if (!suppressed) {
 			defaultHandler(warn);
 		}
 	}
-	
-	_suppressCircularDependencyWarnings(warn){
-		const ignoredCircularFiles = [
-			"Column.js",
-			"Tabulator.js",
-		];
 
-		return ignoredCircularFiles.some(file => warn.importer.includes(file));
+	_suppressCircularDependencyWarnings(warn) {
+		const ignoredCircularFiles = ["Column.js", "Tabulator.js"];
+
+		return ignoredCircularFiles.some((file) => warn.importer.includes(file));
 	}
-	
-	bundle(){
-		if(this.env){
+
+	bundle() {
+		if (this.env) {
 			this.watch(this.env);
-		}else{
+		} else {
 			this.build();
 		}
-		
+
 		return this.bundles;
 	}
-	
-	watch(env){
+
+	watch(env) {
 		console.log("Building Dev Package Bundles: ", env);
-		switch(env){
+		switch (env) {
 			case "css":
-			this.bundleCSS(false);
-			break;
-			
+				this.bundleCSS(false);
+				break;
+
 			case "esm":
-			this.bundleESM(false);
-			break;
-			
+				this.bundleESM(false);
+				break;
+
 			case "umd":
-			this.bundleUMD(false);
-			break;
-			
+				this.bundleUMD(false);
+				break;
+
 			case "wrappers":
-			this.buildWrappers();
-			break;
-			
+				this.buildWrappers();
+				break;
+
 			default:
-			this.bundleCSS(false);
-			this.bundleESM(false);
-			break;
+				this.bundleCSS(false);
+				this.bundleESM(false);
+				break;
 		}
 	}
-	
-	build(){
+
+	build() {
 		console.log("Clearing Dist Files");
-		
+
 		this.clearDist();
-		
+
 		console.log("Building Wrappers");
-		
+
 		this.buildWrappers();
-		
+
 		console.log("Building Production Package Bundles");
-		
+
 		this.bundleCSS(false);
 		this.bundleCSS(true);
-		
+
 		this.bundleESM(false);
 		this.bundleESM(true);
-		
+
 		this.bundleUMD(false);
 		this.bundleUMD(true);
 	}
-	
-	clearDist(){
+
+	clearDist() {
 		fs.emptyDirSync("./dist");
 	}
-	
-	buildWrappers(){
+
+	buildWrappers() {
 		var builds = ["jquery_wrapper.js"];
-		
+
 		builds.forEach((build) => {
 			fs.copySync("./src/js/builds/" + build, "./dist/js/" + build);
 		});
 	}
-	
-	bundleCSS(minify){
-		this.bundles = this.bundles.concat(globbySync("./src/scss/**/tabulator*.scss").map(inputFile => {
-			
-			var file = inputFile.split("/");
-			file = file.pop().replace(".scss", (minify ? ".min" : "") + ".css");
-			
-			return {
-				input: inputFile,
-				output: {
-					file: "./dist/css/" + file,
-					format: "es",
-				},
-				plugins: [
-					postcss({
-						modules: false,
-						extract: true,
-						minimize: minify,
-						sourceMap: true,
-						plugins: [require('postcss-prettify')]
-					}),
-				],
-				onwarn:this._suppressUnnecessaryWarnings.bind(this),
-			};
-		}));
+
+	bundleCSS(minify) {
+		this.bundles = this.bundles.concat(
+			globbySync("./src/scss/**/tabulator*.scss").map((inputFile) => {
+				var file = inputFile.split("/");
+				file = file.pop().replace(".scss", (minify ? ".min" : "") + ".css");
+
+				return {
+					input: inputFile,
+					output: {
+						file: "./dist/css/" + file,
+						format: "es",
+					},
+					plugins: [
+						postcss({
+							modules: false,
+							extract: true,
+							minimize: minify,
+							sourceMap: true,
+							plugins: [require("postcss-prettify")],
+						}),
+					],
+					onwarn: this._suppressUnnecessaryWarnings.bind(this),
+				};
+			})
+		);
 	}
-	
-	bundleESM(minify){
+
+	bundleESM(minify) {
 		this.bundles.push({
-			input:"src/js/builds/esm.js",
+			input: "src/js/builds/esm.js",
 			plugins: [
 				nodeResolve(),
 				minify ? terser() : null,
 				license({
 					banner: {
-						commentStyle:"none",
-						content:this.version,
+						commentStyle: "none",
+						content: this.version,
 					},
 				}),
 			],
 			output: [
 				{
-					file: "dist/js/tabulator_esm" + (minify ? ".min" : "") + ".mjs",
+					file: "dist/js/tabulator_esm" + (minify ? ".min" : "") + ".js",
 					format: "esm",
 					exports: "named",
 					sourcemap: true,
 				},
 			],
-			onwarn:this._suppressUnnecessaryWarnings.bind(this),
+			onwarn: this._suppressUnnecessaryWarnings.bind(this),
 		});
 	}
-	
-	bundleUMD(minify){
+
+	bundleUMD(minify) {
 		this.bundles.push({
-			input:"src/js/builds/usd.js",
+			input: "src/js/builds/usd.js",
 			plugins: [
 				nodeResolve(),
 				minify ? terser() : null,
 				license({
 					banner: {
-						commentStyle:"none",
-						content:this.version,
+						commentStyle: "none",
+						content: this.version,
 					},
 				}),
 			],
@@ -186,7 +187,7 @@ export default class Bundler{
 				exports: "default",
 				sourcemap: true,
 			},
-			onwarn:this._suppressUnnecessaryWarnings.bind(this),
+			onwarn: this._suppressUnnecessaryWarnings.bind(this),
 		});
 	}
 }
