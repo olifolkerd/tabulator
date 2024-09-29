8ba304f91c06ff46d85ef0aa3b67f4e1203311ce
Oli Folkerd <oliver.folkerd@gmail.com>
2024-09-29 14:48:51 +0100

    update dependency register to be able to handle props

--- src/js/core/tools/DependencyRegistry.js
+++ src/js/core/tools/DependencyRegistry.js
@@ -6,20 +6,55 @@ export default class DependencyRegistry extends CoreFeature{
 		super(table);
 		
 		this.deps = {};
+
+		this.props = {
+
+		};
 	}
 	
 	initialize(){
 		this.deps = Object.assign({}, this.options("dependencies"));
 	}
 	
-	lookup(key){
+	lookup(key, prop){
+		if(prop){
+			return this.lookupProp(key, prop);
+		}else{
+			return this.lookupKey(key);
+		}
+	}
+
+	lookupProp(key, prop){
+		var dependency;
+
+		if(this.props[key] && this.props[key][prop]){
+			return this.props[key][prop];
+		}else{
+			dependency = this.lookupKey(key);
+
+			if(dependency){
+				if(!this.props[key]){
+					this.props[key] = {};
+				}
+				
+				this.props[key][prop] = dependency[prop] || dependency;
+				return this.props[key][prop];
+			}
+		}
+	}
+
+	lookupKey(key){
+		var dependency;
+
 		if(this.deps[key]){
-			return this.deps[key];
+			dependency = this.deps[key];
 		}else if(window[key]){
 			this.deps[key] = window[key];
-			return this.deps[key];
+			dependency = this.deps[key];
+		}else{
+			console.error("Unable to find dependency", key, "Please check documentation and ensure you have imported the required library into your project");
 		}
-		
-		console.error("Unable to find dependency", key, "Please check documentation and ensure you have imported the required library into your project");
+
+		return dependency;
 	}
 }
