291523287e198a1dab490dc6ebe6571588b2b611
Antonio Norman <antman3351@users.noreply.github.com>
2024-09-29 17:45:33 +0200

    Add option to reset pagination when last page < current page (#3987)
    * Add option to reset the persisted pagination Initial Page when the dataset is too small when accessing remote data #3962
    
    * Removed space in IF statement
    
    * Moved logic to only affect pagination
    
    * Default action, do nothing just issue a warning
    Renamed option paginationOutOfRange
    option paginationOutOfRange can be a function that returns the page
    internally call this.setPage

--- src/js/modules/Page/Page.js
+++ src/js/modules/Page/Page.js
@@ -43,6 +43,7 @@ export default class Page extends Module{
 		// this.registerTableOption("paginationDataSent", {}); //pagination data sent to the server
 		// this.registerTableOption("paginationDataReceived", {}); //pagination data received from the server
 		this.registerTableOption("paginationAddRow", "page"); //add rows on table or page
+		this.registerTableOption("paginationOutOfRange", false); //reset the current page when the last page < this.page, values: false|function|any value accepted by setPage()
 		
 		this.registerTableOption("progressiveLoad", false); //progressive loading
 		this.registerTableOption("progressiveLoadDelay", 0); //delay between requests
@@ -849,6 +850,21 @@ export default class Page extends Module{
 				
 				return false;
 			}else{
+
+				if(this.page > this.max){
+					console.warn( "Remote Pagination Error - Server returned last page value lower than the current page" );
+
+					const paginationOutOfRange = this.options('paginationOutOfRange');
+
+					if(paginationOutOfRange){
+						return this.setPage(
+							typeof paginationOutOfRange === 'function' ?
+								paginationOutOfRange.call(this, this.page, this.max) :
+								paginationOutOfRange
+						);
+					}
+				}
+
 				// left = this.table.rowManager.scrollLeft;
 				this.dispatchExternal("pageLoaded",  this.getPage());
 				// this.table.rowManager.scrollHorizontal(left);
