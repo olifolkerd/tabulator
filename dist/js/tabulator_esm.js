dfe4b5b93f1ba112242fece5f76a7a90d412052a
Oli Folkerd <oliver.folkerd@gmail.com>
2024-09-29 11:19:08 +0100

    add valueMap to array formatter

--- dist/js/tabulator_esm.js
+++ dist/js/tabulator_esm.js
@@ -10863,8 +10863,31 @@ function adaptable(cell, params, onRendered){
 
 function array$2(cell, formatterParams, onRendered){
 	var delimiter = formatterParams.delimiter || ",",
-	value = cell.getValue();
-	return Array.isArray(value) ? value.join(delimiter) : value;
+	value = cell.getValue(),
+	table = this.table,
+	valueMap;
+	
+	if(formatterParams.valueMap){
+		if(typeof formatterParams.valueMap === "string"){
+			valueMap = function(value){
+				return value.map((item) => {
+					return Helpers.retrieveNestedData(table.options.nestedFieldSeparator, formatterParams.valueMap, item);
+				});
+			};
+		}else {
+			valueMap = formatterParams.valueMap;
+		}
+	}
+
+	if(Array.isArray(value)){
+		if(valueMap){
+			value = valueMap(value);
+		}
+
+		return value.join(delimiter);
+	}else {
+		return value;
+	}
 }
 
 function json$1(cell, formatterParams, onRendered){
