19b9782c2b9bfc8ed88ac23fb57c23b43a0ac055
Jeon <mumberrymountain@gmail.com>
2025-07-06 21:21:29 +0900

    Fix: script error during column resizing on mobile with resizableColumnGuide option enabled (#4757)

--- src/js/modules/ResizeColumns/ResizeColumns.js
+++ src/js/modules/ResizeColumns/ResizeColumns.js
@@ -210,9 +210,20 @@ export default class ResizeColumns extends Module{
 			component.modules.resize.handleEl.style.height = height;
 		}
 	}
+
+	getResizingClientX(e){
+		let x;
+		if (typeof e.clientX !== "undefined") return x = e.clientX;
+
+		const touch = this.table.options.resizableColumnGuide
+			? e.changedTouches?.[0]
+			: e.touches?.[0];
+
+		return x = touch?.clientX;
+	}
 	
 	resize(e, column){
-		var x = typeof e.clientX === "undefined" ? e.touches[0].clientX : e.clientX,
+		var x = this.getResizingClientX(e),
 		startDiff = x - this.startX,
 		moveDiff = x - this.latestX,
 		blockedBefore, blockedAfter;
